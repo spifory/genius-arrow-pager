5abdc361a2161e61a3165aa5e64928393e7a87d7
spifory <spifory@protonmail.com>
2024-10-18 16:12:18 +0200

    fix(index): change tabs to spaces
    I didn't even notice they were tabs in the first place what the fuck?

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
 const isSongPage =
-	!!window.location.href.match(
-		/http(?:s)?:\/\/genius\.com\/[a-z0-9-]*-lyrics/gi,
-	) && !!document.querySelector("[class^=AlbumTracklist]");
+    !!window.location.href.match(
+        /http(?:s)?:\/\/genius\.com\/[a-z0-9-]*-lyrics/gi,
+    ) && !!document.querySelector("[class^=AlbumTracklist]");
 
 const getNextUrl = (songs, prev) => {
-	const currentIndex = songs.indexOf(window.location.pathname);
-	const nextTrack = songs[prev ? currentIndex - 1 : currentIndex + 1];
+    const currentIndex = songs.indexOf(window.location.pathname);
+    const nextTrack = songs[prev ? currentIndex - 1 : currentIndex + 1];
 
-	if (nextTrack) {
-		window.location.pathname = nextTrack;
-	} else {
-		alert("No track comes before or after this");
-	}
+    if (nextTrack) {
+        window.location.pathname = nextTrack;
+    } else {
+        alert("No track comes before or after this");
+    }
 };
 
 const keys = new Set();
 
 document.addEventListener("keydown", (event) => {
-	if (isSongPage) {
-		keys.add(event.key);
-		const songs = [];
-		const tracklist = document.querySelectorAll(
-			"[class^=AlbumTracklist__TrackName]",
-		);
+    if (isSongPage) {
+        keys.add(event.key);
+        const songs = [];
+        const tracklist = document.querySelectorAll(
+            "[class^=AlbumTracklist__TrackName]",
+        );
 
-		for (const track of tracklist) {
-			songs.push(
-				track.querySelector("[class^=StyledLink]")
-					? track.querySelector("[class^=StyledLink]").pathname
-					: window.location.pathname,
-			);
-		}
+        for (const track of tracklist) {
+            songs.push(
+                track.querySelector("[class^=StyledLink]")
+                    ? track.querySelector("[class^=StyledLink]").pathname
+                    : window.location.pathname,
+            );
+        }
 
-		switch (event.key) {
-			case "ArrowRight":
-			case "ArrowLeft":
-				if (keys.size === 1) {
-					getNextUrl(songs, event.key === "ArrowLeft");
-				}
-				break;
-			default:
-				return;
-		}
-	}
+        switch (event.key) {
+            case "ArrowRight":
+            case "ArrowLeft":
+                if (keys.size === 1) {
+                    getNextUrl(songs, event.key === "ArrowLeft");
+                }
+                break;
+            default:
+                return;
+        }
+    }
 });
 
 document.addEventListener("keyup", (event) => {
-	switch (event.key) {
-		case "ArrowLeft":
-		case "ArrowRight":
-			keys.delete(event.key);
-			break;
-		default:
-			return;
-	}
+    switch (event.key) {
+        case "ArrowLeft":
+        case "ArrowRight":
+            keys.delete(event.key);
+            break;
+        default:
+            return;
+    }
 });
